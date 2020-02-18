e12cc1280377a5142d182fedfdfc785892a75e35
Kohelet <kohelet@cyclonecobra.com>
2020-02-18 15:04:10 -0500

    Remvoed arg stuff, added a dummy method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![feature(impl_trait_in_bindings)]
-use clap::{Arg,App};
 
-trait Trait {}
 
-impl Trait for u32 {}
+trait Trait {
+	fn show(&self);
+}
+
+impl Trait for u32 {
+	fn show(&self) {
+		println!("{}", &self);
+	}
+}
 
 fn garage() -> Box<dyn Trait> {
 	Box::new(5)
@@ -11,20 +17,8 @@ fn garage() -> Box<dyn Trait> {
 
 fn main() {
 
-	let args = App::new("plaything")
-		.version("1.0.0")
-		.author("kohelet <kohelet@cyclonecobra.com")
-		.about("Testing command line argument parsing--for now")
-		.arg(Arg::with_name("item")
-				.short("i")
-				.long("item")
-				.takes_value(true)
-				.required(false)
-				.help("Random item!")).get_matches();
-
-	let item  = args.value_of("item").unwrap().to_string();
-
-	println!("Item is {}", item);
     println!("Hello, world!");
     let x: Box<dyn Trait> = garage();
+
+    x.show();
 }
